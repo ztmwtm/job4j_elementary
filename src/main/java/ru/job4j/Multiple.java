e5def1c873d3af8192f92e11416c86e161f16669
ztmwtm <ztmwtm@gmail.com>
2022-09-23 22:54:50 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("1 * %d = %d\n",  i, i);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vladimir Kudriavtcev");
+        System.out.println("30.04.1986");
+    }
+}
