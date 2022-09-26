5f9a88ae9d63d7d7942e3eb24686f886f3502036
ztmwtm <ztmwtm@gmail.com>
2022-09-26 14:13:14 +0300

    4.1. Операторы ветвлений [#3611 #346422]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -4,6 +4,7 @@ public class AlertDivByZero {
     public static void main(String[] args) {
         possibleDiv(4);
         possibleDiv(0);
+        possibleDiv(-1000);
     }
 
     public static void possibleDiv(int number) {
